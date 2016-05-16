4a404b8d47d97b702423303628595c0ada8066c1
Felipe Corrêa <fcsantos.ap@gmail.com>
2016-05-16 00:20:36 -0300

    Update Global.asax.cs

--- CRUDTeste/CRUDTeste.Api/Global.asax.cs
+++ CRUDTeste/CRUDTeste.Api/Global.asax.cs
@@ -11,5 +11,19 @@ namespace CRUDTeste.Api
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
+        
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+
+            //opcional..
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
     }
 }
